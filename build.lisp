f2522edc4c8f4a9b0c2f5005937b511466ad1a1a
Ralith <ralith@gmail.com>
2009-07-13 14:19:07 -0700

    Removed superseded and broken plugin

--- build.lisp
+++
@@ -1,2 +0,0 @@
-(load "loader.lisp")
-(sb-ext:save-lisp-and-die "thune" :executable t :toplevel #'thune:start :purify t)
--- plugins/stdout-log.lisp
+++
@@ -1,5 +0,0 @@
-(plugin
- :name "stdout-log"
- :hooks (irc-message)
- :action #.(lambda (message)
-	     (format t "<- ~a" (raw-message-string message))))
