5b4ff923351c1ac7190a901791eed09ab4cf241d
Ralith <ralith@arbor.localdomain>
2009-09-24 11:04:15 -0700

    Indentation fix>

--- command.lisp
+++ command.lisp
@@ -8,23 +8,23 @@
     (let* ((string (car (last (parameters message))))
            (first-space (position #\Space string))
            (nick (conf-value "nick" *conf*)))
-     (cond
-       ((char= (aref (conf-value "cmdchar" *conf*) 0)
-               (aref string 0))
-        (if first-space
-            (values (subseq string (1+ first-space))
-                    (subseq string 1 first-space))
-            (values ""
-                    (subseq string 1))))
-       ((string= nick (subseq string 0 (length nick)))
-        (when first-space
-          (let ((second-space (position #\Space string
-                                        :start (1+ first-space))))
-            (if second-space
-                (values (subseq string (1+ second-space))
-                        (subseq string (1+ first-space) second-space))
-                (values ""
-                        (subseq string (1+ first-space)))))))))))
+      (cond
+        ((char= (aref (conf-value "cmdchar" *conf*) 0)
+                (aref string 0))
+         (if first-space
+             (values (subseq string (1+ first-space))
+                     (subseq string 1 first-space))
+             (values ""
+                     (subseq string 1))))
+        ((string= nick (subseq string 0 (length nick)))
+         (when first-space
+           (let ((second-space (position #\Space string
+                                         :start (1+ first-space))))
+             (if second-space
+                 (values (subseq string (1+ second-space))
+                         (subseq string (1+ first-space) second-space))
+                 (values ""
+                         (subseq string (1+ first-space)))))))))))
 
 (defun add-command (function)
   (pushnew function *commands*))
@@ -33,10 +33,11 @@
   (let ((func (intern (concatenate 'string
                                    "COMMAND-"
                                    (symbol-name name)))))
-   `(progn
-      (defun ,func ,args ,@body)
-      (add-command (quote ,func)))))
+    `(progn
+       (defun ,func ,args ,@body)
+       (add-command (quote ,func)))))
 
+;; TODO: Parallelize
 (defhandler command-launcher (socket message)
   (multiple-value-bind (args command) (command-args message)
     (declare (ignore args))
