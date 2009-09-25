dd2741fbffc6a216ef48a0d18eea62af9e6fffde
Ralith <ralith@arbor.localdomain>
2009-09-24 19:33:32 -0700

    Added a handler for participating in combos.

---
+++ handlers/combo.lisp
@@ -0,0 +1,28 @@
+(in-package :thune)
+
+(defvar *last-message* ())
+(defvar *repetition-count* ())
+
+(defhandler combo (socket message)
+  (when (string= "PRIVMSG" (command message))
+    (let ((string (car (last (parameters message)))))
+      (let* ((channel (first (parameters message)))
+             (current (assoc channel *last-message*
+                             :test #'string-equal)))
+        (if current
+            (let ((current-count (assoc channel *repetition-count*
+                                        :test #'string-equal)))
+              (if (string= string (cdr current))
+                  (if current-count
+                      (incf (cdr current-count))
+                      (push (cons channel 1) *repetition-count*))
+                  (progn
+                    (setf (cdr current) string)
+                    (when current-count
+                      (setf (cdr current-count) 1))))
+              (when (and current-count
+                         (>= (cdr current-count) 3))
+                (send socket (reply-to message (conf-value "nick" *conf*) string))
+                (setf (cdr current-count) 0)))
+            (push (cons channel string)
+                  *last-message*))))))
