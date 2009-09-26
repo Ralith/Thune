e37d3c653bec0d51242b0be22d23746ac46eb6ba
Ralith <ralith@arbor.localdomain>
2009-09-25 20:29:50 -0700

    Added emote command.

--- commands/misc.lisp
+++ commands/misc.lisp
@@ -8,3 +8,9 @@
 
 (defcommand "zup" (socket message)
   (send socket (reply-to message "zup")))
+
+(defcommand "emote" (socket message)
+  (send socket (reply-to message (format nil "~CACTION ~a~C"
+                                         (code-char 1)
+                                         (command-args message)
+                                         (code-char 1)))))
