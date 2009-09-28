90c212ffefd2752b170cce91f95aa5cf8a831c0a
Ralith <ralith@arbor.localdomain>
2009-09-28 14:52:40 -0700

    Added more information to seen command output; still needs time.

--- commands/seen.lisp
+++ commands/seen.lisp
@@ -2,7 +2,28 @@
 
 (defcommand "seen" (socket message)
   (let* ((nick (nick (prefix message)))
-         (last (assoc nick *seen* :test #'string-equal)))
-    (send socket (reply-to message (if last
-                                       (format nil "I saw ~a recently." nick)
-                                       (format nil "I've never seen ~a." nick))))))
+         (last (cdr (assoc nick *seen* :test #'string-equal))))
+    (send
+     socket
+     (reply-to
+      message
+      (if last
+          (format nil "Last saw ~a ~a"
+                  nick
+                  (cond
+                    ((or (string-equal (command last) "PRIVMSG")
+                         (string-equal (command last) "NOTICE"))
+                     (format nil "speaking in ~a, saying \"~a\"."
+                             (first (parameters last))
+                             (second (parameters last))))
+                    ((string-equal (command last) "PART")
+                     (format nil "leaving ~a, saying ~a."
+                             (first (parameters last))
+                             (if (second (parameters list))
+                                 (format nil "\"~a\"" (second (parameters list)))
+                                 "nothing.")))
+                    ((string-equal (command last) "JOIN")
+                     (format nil "joining ~a." (first (parameters last))))
+                    ((string-equal (command last) "QUIT")
+                     "quitting.")))
+          (format nil "I've never seen ~a." nick))))))
