6d698a3211ac5237abba59a43b212e60ed39214a
Ralith <ralith@arbor.localdomain>
2009-09-29 13:58:17 -0700

    Made use of format-interval in command seen and improved grammer and handling of strange (i.e. <1) values.

--- commands/seen.lisp
+++ commands/seen.lisp
@@ -6,9 +6,9 @@
          (reply))
     (setf reply
           (if last
-              (format nil "Last saw ~a ~a, ~a"
+              (format nil "Last saw ~a ~a ago, ~a"
                       nick
-                      (format nil "~a seconds ago" (- (get-universal-time) (received last)))
+                      (format-interval (- (get-universal-time) (received last)))
                       (cond
                         ((or (string-equal (command last) "PRIVMSG")
                              (string-equal (command last) "NOTICE"))
