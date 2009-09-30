353eeebeb4726ce68d2f3ad4112c356404ef761c
Ralith <ralith@arbor.localdomain>
2009-09-30 16:56:33 -0700

    Fixed a bug in emote formatting for command seen, and specialcased it when called on the bot's own nick>

--- commands/seen.lisp
+++ commands/seen.lisp
@@ -6,38 +6,40 @@
          (last (cdr (assoc nick *seen* :test #'string-equal)))
          (reply))
     (setf reply
-          (if last
-              (format nil "Last saw ~a ~a ago, ~a"
-                      nick
-                      (format-interval (- (get-universal-time) (received last)))
-                      (cond
-                        ((or (string-equal (command last) "PRIVMSG")
-                             (string-equal (command last) "NOTICE"))
-                         (format nil "speaking to ~a, ~a."
-                                 (first (parameters last))
-                                 (if (ctcpp last)
-                                     (if (emotep last)
-                                         (format nil "emoting \"* ~a ~a\""
-                                                 nick
-                                                 (let* ((string (second (parameters last)))
-                                                        (text-start (1+ (position #\Space string))))
-                                                   (when (> (length string) text-start) (subseq string (1- (length string))))))
-                                         (format nil "sending a CTCP \"~a\"" (let ((string (second (parameters last))))
-                                                                               (subseq string 1 (1- (length string))))))
-                                     (format nil "saying \"~a\"" (second (parameters last))))))
-                        ((string-equal (command last) "PART")
-                         (format nil "leaving ~a, saying ~a."
-                                 (first (parameters last))
-                                 (if (second (parameters last))
-                                     (format nil "\"~a\"" (second (parameters last)))
-                                     "nothing.")))
-                        ((string-equal (command last) "JOIN")
-                         (format nil "joining ~a." (first (parameters last))))
-                        ((string-equal (command last) "QUIT")
-                         (format nil "quitting, saying ~a."
-                                 (if (first (parameters last))
-                                     (format nil "\"~a\"" (first (parameters last)))
-                                     "nothing.")))
-                        (t "doing something strange.")))
-              (format nil "I've never seen anyone going by \"~a\"." nick)))
+          (if (string= nick (conf-value "nick" *conf*))
+              "sup"
+              (if last
+                  (format nil "Last saw ~a ~a ago, ~a"
+                          nick
+                          (format-interval (- (get-universal-time) (received last)))
+                          (cond
+                            ((or (string-equal (command last) "PRIVMSG")
+                                 (string-equal (command last) "NOTICE"))
+                             (format nil "speaking to ~a, ~a."
+                                     (first (parameters last))
+                                     (if (ctcpp last)
+                                         (if (emotep last)
+                                             (format nil "emoting \"* ~a ~a\""
+                                                     nick
+                                                     (let* ((string (second (parameters last)))
+                                                            (text-start (1+ (position #\Space string))))
+                                                       (when (> (length string) text-start) (subseq string text-start (1- (length string))))))
+                                             (format nil "sending a CTCP \"~a\"" (let ((string (second (parameters last))))
+                                                                                   (subseq string 1 (1- (length string))))))
+                                         (format nil "saying \"~a\"" (second (parameters last))))))
+                            ((string-equal (command last) "PART")
+                             (format nil "leaving ~a, saying ~a."
+                                     (first (parameters last))
+                                     (if (second (parameters last))
+                                         (format nil "\"~a\"" (second (parameters last)))
+                                         "nothing.")))
+                            ((string-equal (command last) "JOIN")
+                             (format nil "joining ~a." (first (parameters last))))
+                            ((string-equal (command last) "QUIT")
+                             (format nil "quitting, saying ~a."
+                                     (if (first (parameters last))
+                                         (format nil "\"~a\"" (first (parameters last)))
+                                         "nothing.")))
+                            (t "doing something strange.")))
+                  (format nil "I've never seen anyone going by \"~a\"." nick))))
     (send socket (reply-to message reply))))
