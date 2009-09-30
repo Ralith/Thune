587fe11b68c3447d82f062d1f351de00530fc09d
Ralith <ralith@arbor.localdomain>
2009-09-30 14:12:56 -0700

    Added support for listing available commands to command help.

--- commands/help.lisp
+++ commands/help.lisp
@@ -2,7 +2,14 @@
 
 (defcommand "help" (socket message)
   "Supplies documentation for a command"
-  (let ((docs (documentation (find-command (command-args message)) 'function)))
-    (send socket (reply-to message (if docs
-                                       docs
-                                       "No documentation available.")))))
+  (send socket
+        (reply-to
+         message
+         (let ((args (command-args message)))
+           (if (> (length args) 0)
+               (let ((docs (documentation (find-command args) 'function)))
+                 (if docs docs
+                     "No documentation found."))
+               ;; Note: Format iterator yoinked from PCL
+               (format nil "Available commands: ~{~#[~;~a~;~a and ~a~:;~@{~a~#[~;, and ~:;, ~]~}~]~}.  Call help on individual commands for more information."
+                       (mapcar #'car *commands*)))))))
