38221782cb572a67d5dd4f2178cfaa853e12db5f
Ralith <ralith@arbor.localdomain>
2009-10-07 23:03:04 -0700

    Added montezuma-based message logging.

---
+++ handlers/montezuma-logger.lisp
@@ -0,0 +1,25 @@
+(in-package :thune)
+
+(defvar *montezuma-logs*)
+
+(defhandler montezuma-logger (socket message)
+  (declare (ignore socket))
+  (when (null *montezuma-logs*)
+    (setf *montezuma-logs*
+          (make-instance 'montezuma:index
+                         :path (conf-value "logpath"))))
+  (let ((document))
+    (cond
+      ((typep (prefix message) 'user)
+       (push (cons "nick" (nick (prefix message))) document)
+       (push (cons "username" (username (prefix message))) document)
+       (push (cons "host" (host (prefix message))) document))
+      ((typep (prefix message) 'server)
+       (push (cons "host" (host (prefix message))) document)))
+    (push (cons "received"
+                (format nil "~d" (received message))) document)
+    (push (cons "command" (command message)) document)
+    (loop for i from 1 for parameter in (parameters message)
+         do (push (cons (format nil "parameter-~d" i)
+                        parameter) document))
+    (montezuma:add-document-to-index *montezuma-logs* document)))
--- thune.asd
+++ thune.asd
@@ -14,7 +14,8 @@
             ((:file "autojoin")
              (:file "combo")
              (:file "revenge")
-             (:file "seen")))
+             (:file "seen")
+             (:file "montezuma-logger")))
    (:module "commands"
             :depends-on ("command" "package" "handlers")
             :components
