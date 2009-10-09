ace124355da767a87eb887b192cdf9c2538f4d86
Ralith <ralith@arbor.localdomain>
2009-10-08 22:57:02 -0700

    Fixed combo handler and added saner maximum threadpool size.

--- handlers/combo.lisp
+++ handlers/combo.lisp
@@ -6,16 +6,16 @@
 (defhandler combo (channel message)
   (when (string= "PRIVMSG" (command message))
     (let ((string (car (last (parameters message)))))
-      (let* ((channel (first (parameters message)))
-             (current (assoc channel *last-message*
+      (let* ((from (first (parameters message)))
+             (current (assoc from *last-message*
                              :test #'string-equal)))
         (if current
-            (let ((current-count (assoc channel *repetition-count*
+            (let ((current-count (assoc from *repetition-count*
                                         :test #'string-equal)))
               (if (string= string (cdr current))
                   (if current-count
                       (incf (cdr current-count))
-                      (push (cons channel 1) *repetition-count*))
+                      (push (cons from 1) *repetition-count*))
                   (progn
                     (setf (cdr current) string)
                     (when current-count
@@ -24,5 +24,5 @@
                          (>= (cdr current-count) 3))
                 (send channel (reply-to message string))
                 (setf (cdr current-count) 0)))
-            (push (cons channel string)
+            (push (cons from string)
                   *last-message*))))))
--- thune.lisp
+++ thune.lisp
@@ -18,6 +18,7 @@
   "Launches the bot."
   (sanify-output)
   (setf drakma:*drakma-default-external-format* :utf-8)
+  (setf %thread-pool-soft-limit 64)
   (load-conf "thune.conf")
   (let ((socket)
         (input (make-instance 'channel))
@@ -56,12 +57,12 @@
            (error
             (lambda (e)
               (send output (make-message "QUIT" (format nil "Error: ~a" e))))))
-       (loop
-          (let ((message (recv input)))
-            (if message
-                (progn (format t "-> ~a~%" (message->string message))
-                       (call-handlers output message))
-                (return))))))
+        (loop
+           (let ((message (recv input)))
+             (if message
+                 (progn (format t "-> ~a~%" (message->string message))
+                        (call-handlers output message))
+                 (return))))))
     (loop until die do
          (let ((message (recv output)))
            (send-message socket message)
