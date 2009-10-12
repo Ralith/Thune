6efb5d4265d376fbe96f6adad0043ffb20a71e0f
Ralith <ralith@arbor.localdomain>
2009-10-12 14:36:20 -0700

    Made montezuma indexes per-channel.

--- handlers/montezuma-logger.lisp
+++ handlers/montezuma-logger.lisp
@@ -1,25 +1,32 @@
 (in-package :thune)
 
-(defvar *montezuma-logs*)
+(defvar *channel-logs* nil)
 
 (defhandler montezuma-logger (socket message)
   (declare (ignore socket))
-  (unless *montezuma-logs*
-    (setf *montezuma-logs*
-          (make-instance 'montezuma:index
-                         :path (conf-value "logpath"))))
-  (let ((document))
-    (cond
-      ((typep (prefix message) 'user)
-       (push (cons "nick" (nick (prefix message))) document)
-       (push (cons "username" (username (prefix message))) document)
+  (let* ((channel (first (parameters message)))
+         (index (cdr (assoc channel *channel-logs* :test #'string-equal)))
+         (document))
+    (when (and (some (lambda (x) (string-equal (command message) x))
+                     '("PRIVMSG" "NOTICE" "PART"))
+               (char= #\# (aref channel 0)))
+     (unless index
+       (let ((new-index (make-instance 'montezuma:index
+                                          :path
+                                          (format nil "~a/~a"
+                                                  (conf-value 'logpath)
+                                                  channel))))
+         (push (cons channel new-index) *channel-logs*)
+         (setf index new-index)))
+     (when (prefix message)
+       (when (typep (prefix message) 'user)
+         (push (cons "nick" (nick (prefix message))) document)
+         (push (cons "username" (username (prefix message))) document))
        (push (cons "host" (host (prefix message))) document))
-      ((typep (prefix message) 'server)
-       (push (cons "host" (host (prefix message))) document)))
-    (push (cons "received"
-                (format nil "~d" (received message))) document)
-    (push (cons "command" (command message)) document)
-    (loop for i from 1 for parameter in (parameters message)
-         do (push (cons (format nil "parameter-~d" i)
-                        parameter) document))
-    (montezuma:add-document-to-index *montezuma-logs* document)))
+     (push (cons "received"
+                 (format nil "~d" (received message))) document)
+     (push (cons "command" (command message)) document)
+     (loop for i from 1 for parameter in (parameters message)
+        do (push (cons (format nil "parameter-~d" i)
+                       parameter) document))
+     (montezuma:add-document-to-index index document))))
