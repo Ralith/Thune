e060553a0ca390eb90ad767c02bf5b6ee0fbea5e
Ralith <ralith@arbor.localdomain>
2009-10-14 14:35:58 -0700

    Misc. cleanup and bugfix

--- commands/google.lisp
+++ commands/google.lisp
@@ -1,6 +1,7 @@
 (in-package :thune)
 
 (defun scroogle-search (query)
+  "Returns in the first value the first URL search result, and in the second value its title."
   (multiple-value-bind (content code headers uri)
       (drakma:http-request "https://ssl.scroogle.org/cgi-bin/nbbw.cgi"
                            :method :post
@@ -10,26 +11,18 @@
     (declare (ignore code)
              (ignore headers)
              (ignore uri))
-    content))
-
-(defun find-lhtml (lhtml &rest tags)
-  (if tags
-      (apply #'find-lhtml
-             (loop for element in lhtml
-                when (and (listp element)
-                          (eq (first element) (first tags)))
-                return element)
-             (rest tags))
-      lhtml))
-
-(defcommand "google" (channel message)
-  "Replies with the first google result for a given search query."
-  (let* ((page (chtml:parse (scroogle-search (command-args message))
+    (let* ((page (chtml:parse content
                             (chtml:make-lhtml-builder)))
          (result (find-lhtml page :body :blockquote :a))
          (url (second (first (second result))))
          (title (third result)))
     (when (listp title)
-      (setf title) (third title))
+      (setf title (third title)))
+    (values url title))))
+
+(defcommand "google" (channel message)
+  "Replies with the first google result for a given search query."
+  (multiple-value-bind (url title)
+      (scroogle-search (command-args message))
     (send channel (reply-to message (format nil "~a - ~a"
                                             url title)))))
