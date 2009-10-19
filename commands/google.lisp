e6e3e5856a8052dea88ba64620ca7e56535426a3
Ralith <ralith@arbor.localdomain>
2009-10-18 20:22:32 -0700

    Removed unnecessary multiple-value-bind from scroogle-search.

--- commands/google.lisp
+++ commands/google.lisp
@@ -2,28 +2,24 @@
 
 (defun scroogle-search (query)
   "Returns in the first value the first URL search result, and in the second value its title, or NIL if not results found."
-  (multiple-value-bind (content code headers uri)
-      (drakma:http-request "https://ssl.scroogle.org/cgi-bin/nbbw.cgi"
-                           :method :post
-                           :parameters
-                           (list (cons "Gw" query)
-                                 (cons "n" "2")))
-    (declare (ignore code)
-             (ignore headers)
-             (ignore uri))
-    (let* ((page (chtml:parse content
+  (let* ((content (drakma:http-request "https://ssl.scroogle.org/cgi-bin/nbbw.cgi"
+                                       :method :post
+                                       :parameters
+                                       (list (cons "Gw" query)
+                                             (cons "n" "2"))))
+         (page (chtml:parse content
                             (chtml:make-lhtml-builder)))
          (result (find-nested-tag page :body :blockquote :a))
          (url (second (first (second result))))
          (title-markup (cddr result)))
-      (values url
-              (apply #'concatenate 'string
-                     (mapcar (lambda (x)
-                               (if (listp x)
-                                   (progn (assert (eq :b (first x)))
-                                          (third x))
-                                   x))
-                             title-markup))))))
+    (values url
+            (apply #'concatenate 'string
+                   (mapcar (lambda (x)
+                             (if (listp x)
+                                 (progn (assert (eq :b (first x)))
+                                        (third x))
+                                 x))
+                           title-markup)))))
 
 (defcommand "google" (channel message)
   "Replies with the first google result for a given search query."
